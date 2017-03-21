9d73250695b58eb7b18f391141b7c612832caaa9
oracle <stavr@bk.ru>
2017-03-21 12:36:20 +0200

    added debug apk

---
+++ app/src/main/java/ua/itstep/android11/moneyflow/interfaces/IMvpInteractor.java
@@ -0,0 +1,10 @@
+package ua.itstep.android11.moneyflow.interfaces;
+
+/**
+ * Created by Maksim Baydala on 09/03/17.
+ */
+
+public interface IMvpInteractor {
+
+    void loadItems(); //TODO LoaderListener
+}
---
+++ app/src/main/java/ua/itstep/android11/moneyflow/models/Record.java
@@ -0,0 +1,7 @@
+package ua.itstep.android11.moneyflow.models;
+
+/**
+ * Created by Maksim Baydala on 09/03/17.
+ */
+public class Record {
+}
